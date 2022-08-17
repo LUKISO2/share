de9a9da34221b57d42f00b96b557df3c54051835
LUKISO2 <75501566+LUKISO2@users.noreply.github.com>
2022-08-17 14:05:19 +0200

    Edited files to be more to specifications

--- delmatChecker/delmatcheck.py
+++ delmatChecker/delmatcheck.py
@@ -7,7 +7,7 @@ import os
 
 # Hand changable variables
 version = '1.0.0'
-debugLevel = 'info'
+debugLevel = 'debug'
 logs = []
 delmatExtended = []
 
@@ -47,6 +47,7 @@ logger.addHandler(errHandler)
 
 # Automatic mail reporting requirement
 logs.append(f'Application: delmatchecker.py, Version: {version}, Build: Unknown')
+logger.info(f'Application: delmatchecker.py, Version: {version}, Build: Unknown')
 
 # Writes error 
 def writeLog(array):
@@ -65,17 +66,20 @@ def loadEnviron(config):
     if not os.path.isfile(config):
         logger.warn(f'{config} Does not exist')
         return toReturn
-    with open(config, 'r') as conf:
-        for line in conf.readlines():
-            if '=' in line:
-                key, value = line.split('=')
-                key = key.strip().replace('export ', '')
-                value = value.strip()
-                value = os.path.expandvars(value)
-                value = value.replace('"', '').replace('\\', '/').replace('//', '/')
-                os.environ[key] = str(value)
-                toReturn.append([key, value])
-                logger.debug(f'LOADED: {key}={value}')
+    try:
+        with open(config, 'r') as conf:
+            for line in conf.readlines():
+                if '=' in line:
+                    key, value = line.split('=')
+                    key = key.strip().replace('export ', '')
+                    value = value.strip()
+                    value = os.path.expandvars(value)
+                    value = value.replace('"', '').replace('\\', '/').replace('//', '/')
+                    os.environ[key] = str(value)
+                    toReturn.append([key, value])
+                    logger.debug(f'LOADED: {key}={value}')
+    except Exception as e:
+        logger.error(f'{config} Failed to load: {e}')
     return toReturn
 
 # Sets basic and missing env variables
@@ -99,105 +103,115 @@ check(loadEnviron(os.path.join(os.environ['COMMONCONF_DIR'], f'general_{os.envir
 
 # Data processing
 # Loads all paths that DELMAT periodically checks
-for root, dirs, files in os.walk(delmatFolder):
-    for name in files:
-        path = os.path.normpath(os.path.join(root, name))
-        with open(path, 'r') as f:
-            for line in f.readlines():
-                if '=' in line:
-                    splited = line.split('=')
-                    key = splited[0].strip()
-                    value = splited[-1].strip()
-                    value = os.path.expandvars(value)
-                    value = value.replace('"', '').replace('\\', '/').replace('//', '/')
-                    if key == 'HDFS_FEED_DIRECTORY':
-                        delmatExtended.append([os.path.normpath(value), os.path.join(os.path.basename(os.path.dirname(path)), os.path.basename(path))])
-                        logger.debug(f'Found DELMAT folder: {delmatExtended[-1][0]}, in {delmatExtended[-1][1]}')
+for item in os.listdir(delmatFolder):
+    if os.path.isdir(os.path.join(delmatFolder, item)):
+        logger.debug(f'Found delmat folder: {item}')
+        pathe = os.path.join(delmatFolder, item)
+        for item2 in os.listdir(pathe):
+            path = os.path.join(pathe, item2)
+            try:
+                with open(path, 'r') as f:
+                    for line in f.readlines():
+                        if '=' in line:
+                            splited = line.split('=')
+                            key = splited[0].strip()
+                            value = splited[-1].strip()
+                            value = os.path.expandvars(value)
+                            value = value.replace('"', '').replace('\\', '/').replace('//', '/')
+                            if key == 'HDFS_FEED_DIRECTORY':
+                                delmatExtended.append([os.path.normpath(value), os.path.join(os.path.basename(os.path.dirname(path)), os.path.basename(path))])
+                                logger.debug(f'Found DELMAT folder: {delmatExtended[-1][0]}, in {delmatExtended[-1][1]}')
+            except Exception as e:
+                logger.error(f'Failed to open DELMAT {path}')
+                logs.append(f'Failed to open DELMAT {path}')
 
 # Loads all files and filters out the relevant ones
-for root, dirs, files in os.walk(os.environ['CONF_ROOT']):
-    for name in files:
-        configFile = os.path.join(root, name)
-        if not configFile.endswith('.conf') or re.search('jaas.*.conf', configFile):
-            continue
-
-        # Alert logging requirements
-        loggingStr = ''
-        
-        # Loads required variables into env to be used by parser and logging
-        logger.info('Loading config file: ' + configFile)
-        try:
-            config = pyhocon.ConfigFactory.parse_file(configFile)
-        except Exception as e:
-            logger.error(f"{configFile} is either not a valid hocon config file or pyhocon wasn't able to resolve a global variable")
-            logs.append(f"{configFile} is either not a valid hocon config file or pyhocon wasn't able to resolve a global variable")
-            continue
-        os.environ['feed_System'] = str(config.get('feed_System', None))
-        if os.environ['feed_System'] is None:
-            logger.warn('No feed_System defined in config file: ' + configFile)
-            loggingStr += ' No feed_System defined in config file: ' + configFile
-            logs.append(loggingStr.strip())
-            continue
-        os.environ['feed_Name'] = str(config.get('feed_Name', None))
-        if os.environ['feed_Name'] is None:
-            logger.warn('No feed_Name defined in config file: ' + configFile)
-            loggingStr += ' No feed_Name defined in config file: ' + configFile
-            logs.append(loggingStr.strip())
-            continue
-        os.environ['feed_Version'] = str(config.get('feed_Version', None))
-        if os.environ['feed_Version'] is None:
-            logger.warn('No feed_Version defined in config file: ' + configFile)
-            loggingStr += ' No feed_Version defined in config file: ' + configFile
-            logs.append(loggingStr.strip())
-            continue
-        direction = config.get('feed_Direction', None)
-        logger.info(f'feed_Direction: {str(direction)}, feed_System: {os.environ["feed_System"]}, feed_Name: {os.environ["feed_Name"]}, feed_Version: {os.environ["feed_Version"]}')
-        loggingStr += f' feed_Direction: {str(direction)}, feed_System: {os.environ["feed_System"]}, feed_Name: {os.environ["feed_Name"]}, feed_Version: {os.environ["feed_Version"]}'
-        failMail = config.get('feed_FailMail', None)
-        logger.info('feed_FailMail: ' + str(failMail))
-        loggingStr += ' feed_FailMail: ' + str(failMail)
-        author = config.get('feed_Author', 'Unknown')
-        logger.info('feed_Author: ' + str(author))
-        loggingStr += ' feed_Author: ' + str(author)
-
-        # Loads input from files, check for LocalFS input_Type and if they archive to HDFS, if so it check for their existence in DELMAT, raises error if not found
-        input = config.get('input.input_Type', None)
-        if input == 'LocalFS':
-            inputHDFS = config.get('input.archive_HDFS_Path', None)
-            if inputHDFS is None:
+for item in os.listdir(os.environ['CONF_ROOT']):
+    if os.path.isdir(os.path.join(delmatFolder, item)):
+        logger.debug(f'Found delmat folder: {item}')
+        pathe = os.path.join(delmatFolder, item)
+        for item2 in os.listdir(pathe):
+            configFile = os.path.join(pathe, item2)
+            if not configFile.endswith('.conf') or re.search('jaas.*.conf', configFile):
                 continue
-            delmatToCheckPath = os.path.normpath(os.path.expandvars(inputHDFS).replace('"', '').replace('\\', '/').replace('//', '/').split('/p_dt')[0])
-            logger.debug(f'Input HDFS path: {delmatToCheckPath}')
-            passedIn = False
-            for deli in delmatExtended:
-                if re.search(deli[0], delmatToCheckPath):
-                    logger.info(f'Input config {configFile} found in delmat {deli[1]}')
-                    passedIn = True
-                    break
-            if not passedIn:
-                logger.error(f'Input HDFS path {delmatToCheckPath} from {configFile} is not in delmat files, file made by: {author}')
-                inputLoggingStr = loggingStr + f' Input HDFS path {delmatToCheckPath} from {configFile} is not in delmat files, file made by: {author}'
-                logs.append(inputLoggingStr.strip())
 
-        # Does basically the same as input, but checkes ALL outputs for HDFS and raises error if path not found in DELMAT
-        outputs = config.get('output', [])
-        for output in outputs:
-            outputType = output.get('output_Type', None)
-            if outputType == 'HDFS':
-                outputHDFS = config.get('output.output_HDFS_Path', None)
-                if outputHDFS is None:
+            # Alert logging requirements
+            loggingStr = ''
+            
+            # Loads required variables into env to be used by parser and logging
+            logger.info('Loading config file: ' + configFile)
+            try:
+                config = pyhocon.ConfigFactory.parse_file(configFile)
+            except Exception as e:
+                logger.error(f"{configFile} is either not a valid hocon config file or pyhocon wasn't able to resolve a global variable")
+                logs.append(f"{configFile} is either not a valid hocon config file or pyhocon wasn't able to resolve a global variable")
+                continue
+            os.environ['feed_System'] = str(config.get('feed_System', None))
+            if os.environ['feed_System'] is None:
+                logger.warn('No feed_System defined in config file: ' + configFile)
+                loggingStr += ' No feed_System defined in config file: ' + configFile
+                logs.append(loggingStr.strip())
+                continue
+            os.environ['feed_Name'] = str(config.get('feed_Name', None))
+            if os.environ['feed_Name'] is None:
+                logger.warn('No feed_Name defined in config file: ' + configFile)
+                loggingStr += ' No feed_Name defined in config file: ' + configFile
+                logs.append(loggingStr.strip())
+                continue
+            os.environ['feed_Version'] = str(config.get('feed_Version', None))
+            if os.environ['feed_Version'] is None:
+                logger.warn('No feed_Version defined in config file: ' + configFile)
+                loggingStr += ' No feed_Version defined in config file: ' + configFile
+                logs.append(loggingStr.strip())
+                continue
+            direction = config.get('feed_Direction', None)
+            logger.info(f'feed_Direction: {str(direction)}, feed_System: {os.environ["feed_System"]}, feed_Name: {os.environ["feed_Name"]}, feed_Version: {os.environ["feed_Version"]}')
+            loggingStr += f' feed_Direction: {str(direction)}, feed_System: {os.environ["feed_System"]}, feed_Name: {os.environ["feed_Name"]}, feed_Version: {os.environ["feed_Version"]}'
+            failMail = config.get('feed_FailMail', None)
+            logger.info('feed_FailMail: ' + str(failMail))
+            loggingStr += ' feed_FailMail: ' + str(failMail)
+            author = config.get('feed_Author', 'Unknown')
+            logger.info('feed_Author: ' + str(author))
+            loggingStr += ' feed_Author: ' + str(author)
+
+            # Loads input from files, check for LocalFS input_Type and if they archive to HDFS, if so it check for their existence in DELMAT, raises error if not found
+            input = config.get('input.input_Type', None)
+            if input == 'LocalFS':
+                inputHDFS = config.get('input.archive_HDFS_Path', None)
+                if inputHDFS is None:
                     continue
-                hdfsPath = os.path.normpath(os.path.expandvars(outputHDFS).replace('"', '').replace('\\', '/').replace('//', '/'))
-                logger.debug(f'Output hadoop path: {hdfsPath}')
-                passedOut = False
+                delmatToCheckPath = os.path.normpath(os.path.expandvars(inputHDFS).replace('"', '').replace('\\', '/').replace('//', '/').split('/p_dt')[0])
+                logger.debug(f'Input HDFS path: {delmatToCheckPath}')
+                passedIn = False
                 for deli in delmatExtended:
-                    if re.search(deli[0], hdfsPath):
-                        logger.info(f'Output config {configFile} found in delmat {deli[1]}')
-                        passedOut = True
+                    if re.search(deli[0], delmatToCheckPath):
+                        logger.info(f'Input config {configFile} found in delmat {deli[1]}')
+                        passedIn = True
                         break
-                if not passedOut:
-                    logger.error(f'Output HDFS path {hdfsPath} from {configFile} is not in delmat files, file made by: {author}')
-                    outputLoggingStr = loggingStr + f' Output HDFS path {hdfsPath} from {configFile} is not in delmat files, file made by: {author}'
-                    logs.append(outputLoggingStr.strip())
+                if not passedIn:
+                    logger.error(f'Input HDFS path {delmatToCheckPath} from {configFile} is not in delmat files, file made by: {author}')
+                    inputLoggingStr = loggingStr + f' Input HDFS path {delmatToCheckPath} from {configFile} is not in delmat files, file made by: {author}'
+                    logs.append(inputLoggingStr.strip())
+
+            # Does basically the same as input, but checkes ALL outputs for HDFS and raises error if path not found in DELMAT
+            outputs = config.get('output', [])
+            for output in outputs:
+                outputType = output.get('output_Type', None)
+                if outputType == 'HDFS':
+                    outputHDFS = config.get('output.output_HDFS_Path', None)
+                    if outputHDFS is None:
+                        continue
+                    hdfsPath = os.path.normpath(os.path.expandvars(outputHDFS).replace('"', '').replace('\\', '/').replace('//', '/'))
+                    logger.debug(f'Output hadoop path: {hdfsPath}')
+                    passedOut = False
+                    for deli in delmatExtended:
+                        if re.search(deli[0], hdfsPath):
+                            logger.info(f'Output config {configFile} found in delmat {deli[1]}')
+                            passedOut = True
+                            break
+                    if not passedOut:
+                        logger.error(f'Output HDFS path {hdfsPath} from {configFile} is not in delmat files, file made by: {author}')
+                        outputLoggingStr = loggingStr + f' Output HDFS path {hdfsPath} from {configFile} is not in delmat files, file made by: {author}'
+                        logs.append(outputLoggingStr.strip())
 
 writeLog(logs)
