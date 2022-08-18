594d0130b9ec55a9a5bdd10ed3ee26300f4c2dd9
LUKISO2 <75501566+LUKISO2@users.noreply.github.com>
2022-08-18 15:24:01 +0200

    Added threads
    IT STILLS REQUIRES CHANGES

--- delmatChecker/delmatcheck.py
+++ delmatChecker/delmatcheck.py
@@ -1,15 +1,20 @@
 #!/opt/cloudera/parcels/Anaconda/envs/python36/bin/python
+import concurrent.futures
 import logging.handlers
 import configparser
+import subprocess
 import logging
+import queue
 import pyhocon
 import sys
 import re
+import io
 import os
 
 # Hand changable variables
 version = '1.2.1'
 delmatExtended = []
+toDo = queue.Queue()
 
 #temporaly logging before config
 formater = logging.Formatter('%(asctime)s %(levelname)s %(name)s$ [%(processName)s] %(message)s')
@@ -39,8 +44,6 @@ if os.path.isfile(os.path.join(confRoot, config_file)):
 else:
     tmpLog.error('Configuration file not found')
     sys.exit(2)
-
-print(f'Log level is {debugLevel}')
 
 # Set logging
 class MaxLevelFilter(logging.Filter):
@@ -150,9 +153,7 @@ for item in os.listdir(delmatFolder):
 
 # Loads all files and filters out the relevant ones
 for item in os.listdir(os.environ['CONF_ROOT']):
-    logger.debug(f'Loading {item}')
     if os.path.isdir(os.path.join(os.environ['CONF_ROOT'], item)):
-        logger.debug(f'Working with folder {item}')
         pathe = os.path.join(os.environ['CONF_ROOT'], item)
         for item2 in os.listdir(pathe):
             configFile = os.path.join(pathe, item2)
@@ -160,65 +161,89 @@ for item in os.listdir(os.environ['CONF_ROOT']):
                 continue
             if not configFile.endswith('.conf') or re.search('jaas.*.conf', configFile):
                 continue
-            
-            # Loads required variables into env to be used by parser and logging
-            logger.info('Loading config file: ' + configFile)
-            try:
-                config = pyhocon.ConfigFactory.parse_file(configFile)
-            except Exception as e:
-                logger.warn(f"{configFile} is either not a valid hocon config file or pyhocon wasn't able to resolve a global variable, error: {e}")
-                continue
-            os.environ['feed_System'] = str(config.get('feed_System', None))
-            if os.environ['feed_System'] is None:
-                logger.warn('No feed_System defined in config file: ' + configFile)
-                continue
-            os.environ['feed_Name'] = str(config.get('feed_Name', None))
-            if os.environ['feed_Name'] is None:
-                logger.warn('No feed_Name defined in config file: ' + configFile)
-                continue
-            os.environ['feed_Version'] = str(config.get('feed_Version', None))
-            if os.environ['feed_Version'] is None:
-                logger.warn('No feed_Version defined in config file: ' + configFile)
-                continue
-            direction = config.get('feed_Direction', None)
-            logger.info(f'feed_Direction: {str(direction)}, feed_System: {os.environ["feed_System"]}, feed_Name: {os.environ["feed_Name"]}, feed_Version: {os.environ["feed_Version"]}')
-            failMail = config.get('feed_FailMail', None)
-            logger.info('feed_FailMail: ' + str(failMail))
-            author = config.get('feed_Author', 'Unknown')
-            logger.info('feed_Author: ' + str(author))
+            toDo.put(configFile)
 
-            # Loads input from files, check for LocalFS input_Type and if they archive to HDFS, if so it check for their existence in DELMAT, raises error if not found
-            input = config.get('input.input_Type', None)
-            if input == 'LocalFS':
-                inputHDFS = config.get('input.archive_HDFS_Path', None)
-                if inputHDFS is None:
-                    continue
-                delmatToCheckPath = os.path.normpath(os.path.expandvars(inputHDFS).replace('"', '').replace('\\', '/').replace('//', '/').split('/p_dt')[0])
-                logger.debug(f'Input HDFS path: {delmatToCheckPath}')
-                passedIn = False
+def main(configFile, delmatExtended=delmatExtended, logger=logger):
+        # Loads required variables into env to be used by parser and logging
+        logger.info('Loading config file: ' + configFile)
+        try:
+            config = pyhocon.ConfigFactory.parse_file(configFile)
+        except Exception as e:
+            logger.warn(f"{configFile} is either not a valid hocon config file or pyhocon wasn't able to resolve a global variable, error: {e}")
+            return
+        feedSystem = str(config.get('feed_System', None))
+        os.environ['feed_System'] = feedSystem
+        if feedSystem is None:
+            logger.warn('No feed_System defined in config file: ' + configFile)
+            return
+        feedName = str(config.get('feed_Name', None))
+        os.environ['feed_Name'] = feedName
+        if feedName is None:
+            logger.warn('No feed_Name defined in config file: ' + configFile)
+            return
+        feedVersion = str(config.get('feed_Version', None))
+        os.environ['feed_Version'] = feedVersion
+        if feedVersion is None:
+            logger.warn('No feed_Version defined in config file: ' + configFile)
+            return
+        direction = config.get('feed_Direction', None)
+        logger.info(f'feed_Direction: {str(direction)}, feed_System: {feedSystem}, feed_Name: {feedName}, feed_Version: {feedVersion}')
+        failMail = config.get('feed_FailMail', None)
+        logger.info('feed_FailMail: ' + str(failMail))
+        author = config.get('feed_Author', 'Unknown')
+        logger.info('feed_Author: ' + str(author))
+
+        # Loads input from files, check for LocalFS input_Type and if they archive to HDFS, if so it check for their existence in DELMAT, raises error if not found
+        inputHDFS = config.get('input.archive_HDFS_Path', None)
+        if inputHDFS is None:
+            return
+        delmatToCheckPath = os.path.normpath(os.path.expandvars(inputHDFS).replace('"', '').replace('\\', '/').replace('//', '/').split('/p_dt')[0])
+        logger.debug(f'Input HDFS path: {delmatToCheckPath}')
+        answerInp = subprocess.run(['hadoop', 'fs', '-ls', delmatToCheckPath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ansInp = answerInp.stdout.decode('utf-8').strip()
+        passedIn = False
+        if not re.search('/p_', ansInp, flags=re.M|re.S):
+            logger.info(f'No p_* folder found on hadoop path: {ansInp}')
+            return
+        for deli in delmatExtended:
+            if re.search(deli[0], delmatToCheckPath):
+                logger.info(f'Input config {configFile} found in delmat {deli[1]}')
+                passedIn = True
+                break
+        if not passedIn:
+            logger.warn(f'Input HDFS path {delmatToCheckPath} from {configFile} is not in delmat files, file made by: {author}')
+
+        # Does basically the same as input, but checkes ALL outputs for HDFS and raises error if path not found in DELMAT
+        outputs = config.get('output', [])
+        for output in outputs:
+            outputType = output.get('output_Type', None)
+            if outputType == 'HDFS':
+                outputHDFS = config.get('output.output_HDFS_Path', None)
+                if outputHDFS is None:
+                    return
+                hdfsPath = os.path.normpath(os.path.expandvars(outputHDFS).replace('"', '').replace('\\', '/').replace('//', '/'))
+                logger.debug(f'Output hadoop path: {hdfsPath}')
+                answerOut = subprocess.run(['hadoop', 'fs', '-ls', delmatToCheckPath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                ansOut = answerOut.stdout.decode('utf-8').strip()
+                if not re.search('/p_', ansOut, flags=re.M|re.S):
+                    logger.info(f'No p_* folder found on hadoop path: {ansOut}')
+                    return
+                passedOut = False
                 for deli in delmatExtended:
-                    if re.search(deli[0], delmatToCheckPath):
-                        logger.info(f'Input config {configFile} found in delmat {deli[1]}')
-                        passedIn = True
+                    if re.search(deli[0], hdfsPath):
+                        logger.info(f'Output config {configFile} found in delmat {deli[1]}')
+                        passedOut = True
                         break
-                if not passedIn:
-                    logger.error(f'Input HDFS path {delmatToCheckPath} from {configFile} is not in delmat files, file made by: {author}')
+                if not passedOut:
+                    logger.warn(f'Output HDFS path {hdfsPath} from {configFile} is not in delmat files, file made by: {author}')
 
-            # Does basically the same as input, but checkes ALL outputs for HDFS and raises error if path not found in DELMAT
-            outputs = config.get('output', [])
-            for output in outputs:
-                outputType = output.get('output_Type', None)
-                if outputType == 'HDFS':
-                    outputHDFS = config.get('output.output_HDFS_Path', None)
-                    if outputHDFS is None:
-                        continue
-                    hdfsPath = os.path.normpath(os.path.expandvars(outputHDFS).replace('"', '').replace('\\', '/').replace('//', '/'))
-                    logger.debug(f'Output hadoop path: {hdfsPath}')
-                    passedOut = False
-                    for deli in delmatExtended:
-                        if re.search(deli[0], hdfsPath):
-                            logger.info(f'Output config {configFile} found in delmat {deli[1]}')
-                            passedOut = True
-                            break
-                    if not passedOut:
-                        logger.error(f'Output HDFS path {hdfsPath} from {configFile} is not in delmat files, file made by: {author}')
+if __name__ == '__main__':
+    tre = []
+    for item in range(toDo.qsize()):
+        tre.append(toDo.get())
+
+    # BEFORE SETTING max_workers TO ANYTHING BUT 1, you must rewrite file Handlerel to io.SringIO var!
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        executor.map(main, tre)
+
+logger.info('DONE!')
